31650b4e05a834df9cbb5713f48a4c4bfa240933
Rajeshwar Kottala <rajeshwar.kottala@rackspace.com>
2018-06-10 11:20:49 -0500

    First commit

---
+++ src/MyComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class MyComponent extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                <p>My Component!!</p>
+            </div>
+        );
+    }
+
+}
+
+export default MyComponent;
